87b414036aff203972bc2a9477463e9d662b4f30
Harsh <darshnar34@gmail.com>
2024-06-20 17:22:00 +0530

    fixed cburl

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ passport.use(
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/blogs",
+        callbackURL: "https://blog2-q72m.onrender.com/auth/google/blogs",
         userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
       },
       async (accessToken, refreshToken, profile, cb) => {
